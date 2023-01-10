d2dc5864748ee94007b28e64dac2ca9a931bb1ec
Tompa123 <mrguy_93@hotmail.com>
2023-01-10 17:21:18 +0100

    Initial commit

---
+++ src/JframeLayout.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Jframe extends JPanel {
+    private JLabel enterName = new JLabel("Skriv in ditt namn!");
+
+    public Jframe(){
+        StartMeny();
+    }
+
+
+
+
+    public void StartMeny(){
+        Jframe frame = new Jframe("Tärning")
+        JPanel jp = new JPanel();
+        startFrame.add(jp);
+        setSize(600, 510);
+        setVisible(true);
+        setDefaultCloseOperation();
+
+
+    }
+
+    private void setDefaultCloseOperation() {
+    }
+}
